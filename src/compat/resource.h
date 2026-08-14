2309da20de22ab0c5ccf72ad09c20447a3ef8296
igugyj <csy2146047622@gmail.com>
2026-08-14 16:43:04 +0800

    build: improve ver num

--- src/compat/resource.h
+++ src/compat/resource.h
@@ -1,30 +1,30 @@
 ﻿//{{NO_DEPENDENCIES}}
-#define IDD_OPTIONS_DIALOG              101
-#define IDS_PLUGIN_NAME                 103
-#define IDS_PLUGIN_DESCRIPTION          104
-#define IDS_LYRIC_ITEM_NAME             105
-#define IDC_PORT_EDIT                   1001
-#define IDC_WIDTH_EDIT                  1002
-#define IDC_FONT_COMBO                  1003
-#define IDC_SPEED_EDIT                  1004
-#define IDC_FONT_EDIT                   1005
-#define IDC_FONT_BUTTON                 1006
+#define IDD_OPTIONS_DIALOG 101
+#define IDS_PLUGIN_NAME 103
+#define IDS_PLUGIN_DESCRIPTION 104
+#define IDS_LYRIC_ITEM_NAME 105
+#define IDC_PORT_EDIT 1001
+#define IDC_WIDTH_EDIT 1002
+#define IDC_FONT_COMBO 1003
+#define IDC_SPEED_EDIT 1004
+#define IDC_FONT_EDIT 1005
+#define IDC_FONT_BUTTON 1006
 
-#define VER_MAJOR                       0
-#define VER_MINOR                       2
-#define VER_PATCH                       0
+#define VER_MAJOR 0
+#define VER_MINOR 2
+#define VER_PATCH 1
 #ifndef VER_BUILD
-#define VER_BUILD                       0
+#define VER_BUILD 0
 #endif
 
 #define VER_FILE_VERSION_STR "0.2.1"
-#define VER_PRODUCT_VERSION_STR         VER_FILE_VERSION_STR
+#define VER_PRODUCT_VERSION_STR VER_FILE_VERSION_STR
 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_RESOURCE_VALUE 102
+#define _APS_NEXT_COMMAND_VALUE 40001
+#define _APS_NEXT_CONTROL_VALUE 1007
+#define _APS_NEXT_SYMED_VALUE 101
 #endif
 #endif
